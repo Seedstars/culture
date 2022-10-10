a9783a7712f7a944feb145555bffe7501f19afeb
John Idogun <sirneij@Johns-MacBook-Pro.local>
2022-10-10 22:37:30 +0100

    Add semgrep rule for variables in log messages.

--- code/validation/2022.07/targets/python/no-variables-in-log-messages.py
+++ code/validation/2022.07/targets/python/no-variables-in-log-messages.py
@@ -1,4 +1,23 @@
-# ruleid: python.custom-credit-group.no-variables-in-log-messages
 import uuid
 user_id = uuid.uuid4()
+name = 'Jogn'
+username = ''
+detail = {
+    'username': '',
+    'name': 'Jogn',
+    'user_id': user_id
+}
+
+# ruleid: python.custom-credit-group.no-variables-in-log-messages
 django_rest_logger.log.error(f'Account not found for {user_id}')
+
+# ruleid: python.custom-credit-group.no-variables-in-log-messages
+django_rest_logger.log.error(
+    f'Account not found for {user_id}'
+    f'Maybe you misspelt your name {name}'
+    f'Or your username {username} was missing'
+    f'Or, just check this detail: {detail}'
+)
+
+# ok: python.custom-credit-group.no-variables-in-log-messages
+django_rest_logger.log.error('Account not found for', details={'user_id':str(user_id)})
